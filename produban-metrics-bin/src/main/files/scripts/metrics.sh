708d8a3ed9cbb0112300d99227facdbc2ac43d29
guillermoortiz <guillermo.ortiz.fdez@gmail.com>
2015-05-26 13:34:01 +0200

    Fixed scripts to run in a cluster with Spark

--- produban-metrics-bin/src/main/files/scripts/metrics.sh
+++ produban-metrics-bin/src/main/files/scripts/metrics.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 export HADOOP_CONF_DIR=/etc/hadoop/conf
-SPARK_CLASSPATH="/usr/metrics/conf/,"
+SPARK_CLASSPATH="file:/usr/metrics/conf/elasticSearch.properties,file:/usr/metrics/conf/redis.properties,/etc/spark/conf.cloudera.spark_on_yarn/yarn-conf/"
 for lib in `ls /usr/metrics/lib/*.jar`
 do
         if [ -z "$SPARK_CLASSPATH" ]; then
 		SPARK_CLASSPATH=$lib
 	else
-		SPARK_CLASSPATH=$lib,$SPARK_CLASSPATH
+		SPARK_CLASSPATH=$SPARK_CLASSPATH,$lib
 	fi
 done
-
-spark-submit --name "Metrics" --master yarn-client --class com.produban.metrics.MetricsSpark --jars $SPARK_CLASSPATH --executor-memory 1g /usr/metrics/ex/produban-metrics-spark.jar
+spark-submit --name "Metrics" --master yarn-client --class com.produban.metrics.MetricsSpark --jars $SPARK_CLASSPATH --executor-memory 1g /usr/metrics/ex/produban-metrics-spark.jar $1 $2
--- produban-metrics-bin/src/main/files/scripts/metricsInternational.sh
+++ produban-metrics-bin/src/main/files/scripts/metricsInternational.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export HADOOP_CONF_DIR=/etc/hadoop/conf
-SPARK_CLASSPATH="/usr/metrics/conf/,"
+SPARK_CLASSPATH="file:/usr/metrics/conf/elasticSearch.properties,file:/usr/metrics/conf/redis.properties,/etc/spark/conf.cloudera.spark_on_yarn/yarn-conf/"
 for lib in `ls /usr/metrics/lib/*.jar`
 do
         if [ -z "$SPARK_CLASSPATH" ]; then
 		SPARK_CLASSPATH=$lib
 	else
-		SPARK_CLASSPATH=$lib,$SPARK_CLASSPATH
+		SPARK_CLASSPATH=$SPARK_CLASSPATH,$lib
 	fi
 done
 
