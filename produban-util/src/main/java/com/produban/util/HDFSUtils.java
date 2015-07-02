6ccf95131550f14a288034acb8742a18871e21cc
guillermoortiz <guillermo.ortiz.fdez@gmail.com>
2015-07-02 14:29:44 +0200

    Add kerberos when you read from hdfs

--- produban-util/src/main/java/com/produban/util/HDFSUtils.java
+++ produban-util/src/main/java/com/produban/util/HDFSUtils.java
@@ -3,7 +3,6 @@ package com.produban.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +10,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.log4j.Logger;
 
 /**
@@ -21,7 +21,7 @@ public final class HDFSUtils {
 	/**
 	 * Instance of the log object.
 	 */
-	private static final Logger LOG = Logger.getLogger(HDFSUtils.class);	
+	private static final Logger LOG = Logger.getLogger(HDFSUtils.class);
 
 	/**
 	 * Private constructor, only static methods.
@@ -44,12 +44,12 @@ public final class HDFSUtils {
 			BufferedReader bufferedReader = new BufferedReader(
 					new InputStreamReader(fileSystem.open(path)));
 			String line = bufferedReader.readLine();
-			while (line != null) {				
+			while (line != null) {
 				lines.add(line);
 				line = bufferedReader.readLine();
 			}
 		} catch (IOException e) {
-			LOG.error("Error reading file " + pathFile, e);			
+			LOG.error("Error reading file " + pathFile, e);
 		}
 		return lines;
 	}
@@ -63,19 +63,60 @@ public final class HDFSUtils {
 
 			FileStatus status = fileSystem.getFileStatus(path);
 			modificationTime = status.getModificationTime();
-			
+
 		} catch (IOException e) {
-			LOG.error("Error reading file " + pathFile, e);		
+			LOG.error("Error reading file " + pathFile, e);
 		}
 		return modificationTime;
 	}
-	
 
-	public static void main(String[] args) throws URISyntaxException {
-		List<String> file = HDFSUtils
-		//		.readHDFS("/tmp/b.txt");
-		 .readHDFS("/user/ingesta/PRO/landing/PAIS/part-m-00000");
+	public static List<String> readHDFS(final String pathFile,
+			final String principal, final String ticketKerberosPath) {
+		List<String> lines = new ArrayList<String>();
+		try {
+			Path path = new Path(pathFile);
+			Configuration conf = new Configuration();
+			conf.set("hadoop.security.authentication", "Kerberos");
+			UserGroupInformation.setConfiguration(conf);
+			UserGroupInformation.loginUserFromKeytab(principal,
+					ticketKerberosPath);
+			FileSystem fileSystem = FileSystem.get(conf);
 
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(fileSystem.open(path)));
+			String line = bufferedReader.readLine();
+			while (line != null) {
+				lines.add(line);
+				line = bufferedReader.readLine();
+			}
+		} catch (IOException e) {
+			LOG.error("Error reading file " + pathFile, e);
+		}
+		return lines;
+	}
+
+	public static long lastModification(final String pathFile,
+			final String principal, final String ticketKerberosPath) {
+		long modificationTime = 0l;
+		try {
+			Path path = new Path(pathFile);
+			Configuration conf = new Configuration();
+			conf.set("hadoop.security.authentication", "Kerberos");
+			UserGroupInformation.setConfiguration(conf);
+			UserGroupInformation.loginUserFromKeytab(principal,
+					ticketKerberosPath);			
+			// UserGroupInformation.loginUserFromKeytab("example_user@IBM.COM",
+			// "/path/to/example_user.keytab");
+
+			FileSystem fileSystem = FileSystem.get(conf);
+
+			FileStatus status = fileSystem.getFileStatus(path);
+			modificationTime = status.getModificationTime();
+
+		} catch (IOException e) {
+			LOG.error("Error reading file " + pathFile, e);
+		}
+		return modificationTime;
 	}
 
 }
